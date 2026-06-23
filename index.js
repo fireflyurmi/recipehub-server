0259e72a06bddcf5553f0a171fd42e1aff64e2dc
unknown <halderurmi@gmail.com>
2026-06-23 13:55:41 +0600

    MongoDB, cors, dotenv, Express installation has done

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ dotenv.config();
 const uri = process.env.MONGO_DB_URI;
 const app = express()
 const port = process.env.PORT;
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send("RecipeHub Server is running fine !!!")
 })
 
 
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -28,14 +28,12 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
